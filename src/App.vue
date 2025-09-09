4b32867f7de30963c374fa2b1a001d9dd5e2de75
PatrikasDapsys <patrikas.dapsys@gmail.com>
2025-09-09 13:33:36 +0300

    setup

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,21 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <main id="app">
+    <div class="test">
+      <div class="test2">AHAHH</div>
+      wassup
+      <i class="fa-solid fa-house"></i>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
   </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<style scoped lang="scss">
+.test {
+  background: red;
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+  .test2 {
+    color: blue;
   }
 }
 </style>
